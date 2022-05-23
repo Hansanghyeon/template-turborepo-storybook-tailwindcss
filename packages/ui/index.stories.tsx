bead3264fa12529be0b5e5d2848d98e9d73292d8
999 <hyeon@imweb.me>
2022-05-23 10:38:37 +0900

    feat: ui storybook

---
+++ packages/ui/index.stories.tsx
@@ -0,0 +1,10 @@
+import { ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+export const Template = () => <Button />;
